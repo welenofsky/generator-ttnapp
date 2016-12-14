dd795a772aa9ccdceec91b41f376078404430369
Justin Welenofsky <welenofsky@gmail.com>
2016-12-13 20:52:39 -0800

    Update yeoman test

--- test/app.js
+++ test/app.js
@@ -4,10 +4,11 @@ var assert = require('yeoman-assert');
 var helpers = require('yeoman-test');
 
 describe('generator-ttnapp:app', function () {
-  before(function () {
+  beforeEach(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
-      .toPromise();
+      .withOptions({foo: 'bar'})    // Mock options passed in
+      .withArguments(['name-x'])      // Mock the arguments
+      .withPrompts({someAnswer: true});
   });
 
   it('creates files', function () {
